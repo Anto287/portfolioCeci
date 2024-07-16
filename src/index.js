cef0ec7c2488c6bc16824efadeb7e45cb06ff667
Antonio <contriantoni@gmail.com>
2024-07-16 22:13:13 +0200

    Spero di sistemare il deploy o mi ammazzo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+const App = React.lazy(() => import('./App'));
 import '@styles/App.css';
+import myIcon from '@assets/icon/icon.png';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function Index(){
-  return(
+const basename = process.env.BASENAME;
+
+function Index() {
+  return (
     <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+      <BrowserRouter basename={basename}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </React.StrictMode>
-  )
-};
+  );
+}
+
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = myIcon;
+document.head.appendChild(link);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
