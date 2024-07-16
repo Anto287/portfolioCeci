40ba829feb2b7bb964b64df485040c78a91a42b0
Antonio <contriantoni@gmail.com>
2024-07-16 22:02:06 +0200

    commit for test deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,76 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.[contenthash].js',
+      publicPath: isProduction ? '/portafoglioCeci/' : '/'
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    mode: isProduction ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'images/',
-            },
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash].[ext]',
+                outputPath: 'images/',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.json$/, 
+          use: ['json-loader']
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
         '@components': path.resolve(__dirname, 'src/components'),
         '@pages': path.resolve(__dirname, 'src/pages'),
         '@styles': path.resolve(__dirname, 'src/styles'),
         '@assets': path.resolve(__dirname, 'src/assets'),
-        '@images': path.resolve(__dirname, 'src/assets/img'),
+        '@images': path.resolve(__dirname, 'src/assets/images'),
+      },
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }), 
+    ],
+    devServer: {
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'build'),
+      },
+      compress: true,
+      port: 3000,
     },
-    compress: true,
-    port: 3000,
-  },
+  };
 };
